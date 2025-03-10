fc30c6f9617c134073d0f35355f8a3d217a4272b
Abhishek-3735 <kopperiabhishek@gmail.com>
2025-03-10 21:02:45 +0530

    Update Order.jsx

--- src/Order.jsx
+++ src/Order.jsx
@@ -5,7 +5,7 @@ const Order = () => {
     <div className="relative flex items-center justify-center p-4">
       <a
         href="#"
-        className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-transparent bg-gradient-to-r from-gray-500 via-white to-gray-500 bg-clip-text
+        className="text-4xl sm:text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-transparent bg-gradient-to-r from-gray-500 via-white to-gray-500 bg-clip-text
          animate-[shine_2s_linear_infinite] px-6 sm:px-8 md:px-10 lg:px-12 py-2 sm:py-3 md:py-4 whitespace-nowrap text-center"
       >
         Go Green Now...
