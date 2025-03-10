642046a5b63cee23846f73b3ff16f6476b7a4e8f
Abhishek-3735 <kopperiabhishek@gmail.com>
2025-03-10 21:07:30 +0530

    Update Button.jsx
    updated text size in Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
     <div className="relative flex items-center justify-center p-4">
       <a
         href="#"
-        className="text-lg sm:text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-transparent bg-gradient-to-r from-gray-500 via-white to-gray-500 bg-clip-text
+        className="text-lg sm:text-xl md:text-4xl lg:text-5xl font-bold pb-5 text-transparent bg-gradient-to-r from-gray-500 via-white to-gray-500 bg-clip-text
          animate-[shine_3s_linear_infinite] px-6 sm:px-8 md:px-10 lg:px-12 py-2 sm:py-3 md:py-4 whitespace-nowrap text-center"
       >
         Designing Vibrant & Eco-Friendly Workspaces!
